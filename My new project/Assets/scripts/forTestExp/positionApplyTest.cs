270525e515690c600965a342f053aa25cf6f05b0
LouislarOldLaptop <a8945267@gmail.com>
2022-09-03 23:56:35 +0800

    測試left front kick stream linear mapping在real time的效果.
    發現, 動作很不明顯.
    比較怪異的點是, 我食指往後轉, 左腿反而更踢出去, 相較於我把食指往前轉
    
    TODO: real time將mapping結果visualize是有必要的

--- My new project/Assets/scripts/forTestExp/positionApplyTest.cs
+++ My new project/Assets/scripts/forTestExp/positionApplyTest.cs
@@ -121,7 +121,7 @@ public class positionApplyTest : MonoBehaviour
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/runSprintLinearMapping_generic_allLeft_TTTTTT_EWMA.json";
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/runSprintStreamLinearMapping_EWMA.json";
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftSideKick_TTTFFF_EWMA.json";
-        readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftSideKick_generic_TTTFFF_EWMA.json";
+        //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftSideKick_generic_TTTFFF_EWMA.json";
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftSideKickLinearMapping_generic_TTTFFF_EWMA.json";
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftSideKickLinearMapping_TTTTTT_EWMA.json";
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftSideKickLinearMapping_EWMA.json";
@@ -129,6 +129,7 @@ public class positionApplyTest : MonoBehaviour
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftFrontKick_EWMA.json";
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftFrontKick_stream_EWMA.json";
         //readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftFrontKickStreamLinearMapping_EWMA.json";
+        readInHumanPositionFile = "jsonPositionData/afterSynthesis/leftFrontKickStreamLinearMapping_TFFTTT_EWMA.json";
         readInHumanPositionResult = jsonDeserializer.readAndParseRotation(
             readInHumanPositionFile
         );
